6a16357e04a1b58248ecc6d4821e7487b533b91f
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-03-18 21:19:05 -0400

    feat: building styling resources

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,11 @@ from cars.models import Car
 # Create your views here.
 
 def cars_view(request):
-    cars=Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars=Car.objects.filter(model__icontains = search).order_by('model')#brand = id
 
     return render(
         request, 
