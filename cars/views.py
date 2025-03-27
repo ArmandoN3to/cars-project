3c52b17d0e30b9e240486f0f556957bbcfbf1cce
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-03-27 11:10:50 -0400

    feat:class based views

--- cars/views.py
+++ cars/views.py
@@ -1,29 +1,36 @@
-from django.shortcuts import render,redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views.generic import ListView ,CreateView, DetailView, UpdateView
 
 # Create your views here.
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search=self.request.GET.get('search')
+        if search:
+            cars=cars.filter(model__icontains=search)
+        return cars
 
-    if search:
-        cars=Car.objects.filter(model__icontains = search).order_by('model')#brand = id
+    
+class NewCarCreateView(CreateView):
+    model = Car
+    template_name = 'new_car.html'
+    form_class = CarModelForm
+    success_url = '/cars/'
 
-    return render(
-        request, 
-        'cars.html',
-        {'cars':cars}
-    )
-def new_car_view(request):
-    if request.method =='POST':
-        new_car_form=CarModelForm(request.POST,request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form=CarModelForm()
-    return render(
-        request,'new_car.html',{'new_car_form':new_car_form}
-    )
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    context_object_name = 'car'
+
+class CarUpdateView(UpdateView):
+    model = Car
+    template_name = 'car_update.html'
+    form_class = CarModelForm
+    success_url = '/cars/'
