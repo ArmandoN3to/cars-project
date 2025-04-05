35db7ff120e5c9a0fbfb1e372954a5e3e07e345a
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-04-05 11:00:24 -0400

    Update client.py

--- llm_api/client.py
+++ llm_api/client.py
@@ -16,40 +16,40 @@ def get_car_bio(model,brand,year):
 
 
 # class Llama():
-    def __init__(self,model,brand,year):
-        self.model = model
-        self.brand = brand
-        self.year = year
-        pass
+    # def __init__(self,model,brand,year):
+    #     self.model = model
+    #     self.brand = brand
+    #     self.year = year
+    #     pass
     
-    # Definindo a URL e os cabeçalhos
-    url = "http://localhost:1234/v1/chat/completions"
-    headers = {"Content-Type": "application/json"}
+    # # Definindo a URL e os cabeçalhos
+    # url = "http://localhost:1234/v1/chat/completions"
+    # headers = {"Content-Type": "application/json"}
     
-    data = {
-        "model": "llama-3.2-1b-instruct",
-        "messages": [
+    # data = {
+    #     "model": "llama-3.2-1b-instruct",
+    #     "messages": [
             
-            {"role": "user",
-                "content": "Me mostre uma descriçao de venda para o carro {self.model} da marca {self.brand} do ano {self.year} em apenas 250 caracteres"}
+    #         {"role": "user",
+    #             "content": "Me mostre uma descriçao de venda para o carro {self.model} da marca {self.brand} do ano {self.year} em apenas 250 caracteres"}
                 
-        ],
-        "temperature": 0.7,
-        "max_tokens": -1,
-        "stream": False
-    }
+    #     ],
+    #     "temperature": 0.7,
+    #     "max_tokens": -1,
+    #     "stream": False
+    # }
 
-    def api_get(self):
-        # Fazendo a requisição POST
-        print("Consumindo LLM...")
-        response = requests.post(
-            Llama.url, headers=Llama.headers, json=Llama.data)
-        content = response.json()
-        content = content.get('choices')[0].get('message').get('content')
-        # Verificando e exibindo a resposta
-        if response.status_code == 200:
-            return content
-        else:
-            print("Erro:", response.status_code, response.text)
+    # def api_get(self):
+    #     # Fazendo a requisição POST
+    #     print("Consumindo LLM...")
+    #     response = requests.post(
+    #         Llama.url, headers=Llama.headers, json=Llama.data)
+    #     content = response.json()
+    #     content = content.get('choices')[0].get('message').get('content')
+    #     # Verificando e exibindo a resposta
+    #     if response.status_code == 200:
+    #         return content
+    #     else:
+    #         print("Erro:", response.status_code, response.text)
 
 
