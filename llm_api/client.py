87301ef2eb7740f9a1267d9eba086b9ab77e7b4a
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2025-04-05 11:55:06 -0400

    Update client.py

--- llm_api/client.py
+++ llm_api/client.py
@@ -1,10 +1,15 @@
 from google import genai
+from dotenv import load_dotenv
 
 
 
 def get_car_bio(model,brand,year):
 
-    client = genai.Client(api_key="Sua chave aqui")
+    load_dotenv()
+
+    api_key = os.getenv('API_KEY')
+
+    client = genai.Client(api_key)
     # Definindo o modelo e o prompt
 
     response = client.models.generate_content(
@@ -13,43 +18,5 @@ def get_car_bio(model,brand,year):
     )
     return response.text
 
-
-
-# class Llama():
-    # def __init__(self,model,brand,year):
-    #     self.model = model
-    #     self.brand = brand
-    #     self.year = year
-    #     pass
-    
-    # # Definindo a URL e os cabeçalhos
-    # url = "http://localhost:1234/v1/chat/completions"
-    # headers = {"Content-Type": "application/json"}
-    
-    # data = {
-    #     "model": "llama-3.2-1b-instruct",
-    #     "messages": [
-            
-    #         {"role": "user",
-    #             "content": "Me mostre uma descriçao de venda para o carro {self.model} da marca {self.brand} do ano {self.year} em apenas 250 caracteres"}
-                
-    #     ],
-    #     "temperature": 0.7,
-    #     "max_tokens": -1,
-    #     "stream": False
-    # }
-
-    # def api_get(self):
-    #     # Fazendo a requisição POST
-    #     print("Consumindo LLM...")
-    #     response = requests.post(
-    #         Llama.url, headers=Llama.headers, json=Llama.data)
-    #     content = response.json()
-    #     content = content.get('choices')[0].get('message').get('content')
-    #     # Verificando e exibindo a resposta
-    #     if response.status_code == 200:
-    #         return content
-    #     else:
-    #         print("Erro:", response.status_code, response.text)
 
 
